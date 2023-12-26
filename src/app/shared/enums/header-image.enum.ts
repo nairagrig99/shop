066dd23259f16ff04183e6d0496ee8abc18ecdbb
Naira Grigoryan <nar.grig.1999@mail.ru>
2023-12-27 00:07:41 +0400

    bug fix

---
+++ src/app/shared/enums/header-image.enum.ts
@@ -0,0 +1,7 @@
+export enum HeaderImageEnum {
+  'SNOW' = 'snow.webp',
+  'VALUES' = 'Thesus-sustainable-values.webp',
+  'BESTSELLERS' = 'Beige_Collection_Page.webp',
+  'RAIN' = 'rain.webp',
+  'WEEKEND' = 'weekend.webp',
+}
--- src/app/shared/store/product/product.effect.ts
+++ src/app/shared/store/product/product.effect.ts
@@ -2,23 +2,37 @@ import {Injectable} from "@angular/core";
 import {ProductsAction, ProductsActionGetSuccess} from "./products.action";
 import {ProductsApiService} from "../../api/products-api.service";
 import {Actions, createEffect, ofType} from '@ngrx/effects';
-import {switchMap} from "rxjs";
+import {filter, of, switchMap} from "rxjs";
 import {map} from "rxjs/operators";
+import {LocalStorageService} from "../../api/localstorage.service";
 
 @Injectable()
 
 export class ProductEffect {
-  constructor(private action$: Actions, private productsApiService: ProductsApiService) {
+  constructor(private action$: Actions,
+              private productsApiService: ProductsApiService,
+              private localStorageService: LocalStorageService) {
   }
 
   public getProductsList$ = createEffect(() => this.action$.pipe(
       ofType(ProductsAction.Get),
       switchMap(() => {
-          return this.productsApiService.getProductList().pipe(
-            map((request) => {
-              return new ProductsActionGetSuccess(request["products"])
-            })
-          )
+          const productFromLocalStrg = this.localStorageService.getLocalStorageItem('products')
+          const objs = Object.keys(productFromLocalStrg);
+          if (objs?.length > 0) {
+            return of(this.localStorageService.getLocalStorageItem('products')).pipe(
+              filter((product) => !!product),
+              map((request) => {
+                return new ProductsActionGetSuccess(request)
+              })
+            )
+          } else {
+            return this.productsApiService.getProductList().pipe(
+              map((request) => {
+                return new ProductsActionGetSuccess(request["products"])
+              })
+            )
+          }
         }
       )
     ) as any
